e8fa6b88d23498bd3a22b4aab69945837465959a
Sarcovora <evan.kuo.edu@gmail.com>
2022-07-21 17:04:07 -0500

    Updated notebook and added get_descriptors.py

---
+++ CogBook/get_descriptors.py
@@ -0,0 +1,21 @@
+from facenet_models import FacenetModel
+
+def get_descriptors(image_data):
+    """
+    Returns a descriptors numpy array.
+
+    Parameters
+    ----------
+    image_data : numpy.ndarray, shape-(R, C, 3) (RGB is the last dimension)
+        Pixel information for the image.
+    
+    Returns
+    -------
+    np.ndarray, shape-(N, 512)
+        The descriptor vectors, where N is the number of faces.
+    """
+    
+    model = FacenetModel()
+    boxes, probabilities, _ = model.detect(image_data)
+    descriptors = model.compute_descriptors(image_data, boxes)
+    return descriptors
