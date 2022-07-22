9b887821f3479b9f2e6dac4b1bf4535c2e9d8957
amritapasu <75818587+amritapasu@users.noreply.github.com>
2022-07-22 07:13:48 -0700

    full whiskers algorithm added, but not tested yet

---
+++ CogBook/whiskers.py
@@ -0,0 +1,61 @@
+import numpy as np
+import random
+from pathlib import Path
+
+"""
+The full whiskers algorithm
+Input: takes in the number of iterations, the list of nodes, and the adjacency matrix
+Output: doesn't return anything, but prints the grouping history at the end 
+        and organizes the images based on groupings
+
+"""
+def whiskers(iterations: int, nodes: list, adj_matrix: np.array):
+    
+    # the algorithm
+    num_grouping_history = []
+    for i in range(0, iterations):
+        node = nodes[random.randint(0, len(nodes)-1)]
+        propagate_label(node, node.neighbors, adj_matrix)
+        groupings = connected_components(nodes)
+        num_grouping_history.append(len(groupings))
+    print(num_grouping_history)
+    
+    # moves the images into folders based on their groupings
+    cwd = Path.cwd()
+    for group_num in range(0, len(groupings)):
+        
+        # makes a folder to hold all the images in this group
+        person_id = 'Person' + str(group_num+1)
+        new_folder_path = cwd / person_id
+        
+        if Path.exists(new_folder_path):
+            print("remove the folders from the previous run")
+        else:
+            new_folder_path.mkdir()
+            
+        # moves the images in this group
+        for node in groupings[group_num]:
+            file_path = node.file_path
+            file_name = Path(file_path).name
+            
+            new_file_path = new_folder_path / file_name
+            print(new_file_path)
+            Path(file_path).rename(new_file_path)
+
+def file_move_test(file_path: str, group_num: int):
+    cwd = Path.cwd()
+    person_id = 'Person' + str(group_num+1)
+    new_folder_path = cwd / person_id
+    
+    print(new_folder_path)
+    if Path.exists(new_folder_path):
+            print("remove the folders from the previous run")
+    else:
+        new_folder_path.mkdir()
+    
+    file_name = Path(file_path).name
+    print(file_name)
+    
+    new_file_path = new_folder_path / file_name
+    print(new_file_path)
+    Path(file_path).rename(new_file_path)
