3f08fbcb26c6627f13bbfb84cb11670af376fa3b
caasib <107368103+caasib@users.noreply.github.com>
2022-07-22 16:49:25 -0400

    fixed connected components

--- CogBook/connected_components.py
+++ CogBook/connected_components.py
@@ -1,7 +1,8 @@
 def connected_components(list_of_nodes):
     connected_nodes = {}
     for node in list_of_nodes:
-        connected_nodes[node.label] = []
+        if node.label not in connected_nodes:
+            connected_nodes[node.label] = []
         for key in connected_nodes:
             if node.label == key:
                 connected_nodes[key].append(node)
