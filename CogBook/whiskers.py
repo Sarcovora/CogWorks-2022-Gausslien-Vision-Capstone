32a354a89f30ad70895d29ecb4fe81683d037afe
amritapasu <75818587+amritapasu@users.noreply.github.com>
2022-07-22 15:47:30 -0700

    even more documentation

--- CogBook/whiskers.py
+++ CogBook/whiskers.py
@@ -65,7 +65,13 @@ def file_move_test(file_path: str, group_num: int):
 
     new_file_path = new_folder_path / file_name
     Path(file_path).rename(new_file_path)
-    
+
+"""
+Moves the images back to the original folder
+Parameters:
+    num_folders (int): the number of folders created by the last run of the whispers algorithms,
+                       needs to be from Folder 1 to Folder num_folders
+"""
 def move_back(num_folders: int):
     cwd = Path.cwd()
     for group_num in range(1, num_folders+1):
