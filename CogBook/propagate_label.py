1b3b2c0a4325360816c7c50c198b75e4cc9627c9
caasib <107368103+caasib@users.noreply.github.com>
2022-07-22 15:51:35 -0400

    edited propagate label function

--- CogBook/propagate_label.py
+++ CogBook/propagate_label.py
@@ -3,12 +3,10 @@ import random
 def propagate_label(node, neighbors, adjacency_matrix):
     # neighbors should be an iterable
     # returns nothing, just updates in place
-    highest_weight = 0
+    edge_weights = {}
     for neighbor in neighbors:
-        print(neighbor)
-        if adjacency_matrix[node.id][neighbor.id] > highest_weight:
-            highest_weight = adjacency_matrix[node.id][neighbor.id]
-            node.label = neighbor.label
-        elif adjacency_matrix[node.id][neighbor.id] == highest_weight:
-            if random.random() > 0.5:
-                node.label = neighbor.label
+        edge_weights[neighbor.label] = adjacency_matrix[node.id][neighbor.id]
+        for key in edge_weights:
+            if neighbor.label == key:
+                edge_weights[key] = edge_weights.get(neighbor.label) + adjacency_matrix[node.id][neighbor.id]
+    node.label = max(edge_weights, key=edge_weights.get)
